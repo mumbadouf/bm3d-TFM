833d40e09b7af5517ddc36f75c01a0d39ff7dcb3
HassanBoutebel0 <h.boutebel@complynow.eu>
2023-04-20 15:26:56 +0200

    Initial commit

---
+++ build/CMakeFiles/bm3d.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hassan/Desktop/TFM/bm3d-master/iio.c" "/home/hassan/Desktop/TFM/bm3d-master/build/CMakeFiles/bm3d.dir/iio.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../PUBLIC"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hassan/Desktop/TFM/bm3d-master/bm3d.cpp" "/home/hassan/Desktop/TFM/bm3d-master/build/CMakeFiles/bm3d.dir/bm3d.cpp.o"
+  "/home/hassan/Desktop/TFM/bm3d-master/lib_transforms.cpp" "/home/hassan/Desktop/TFM/bm3d-master/build/CMakeFiles/bm3d.dir/lib_transforms.cpp.o"
+  "/home/hassan/Desktop/TFM/bm3d-master/main.cpp" "/home/hassan/Desktop/TFM/bm3d-master/build/CMakeFiles/bm3d.dir/main.cpp.o"
+  "/home/hassan/Desktop/TFM/bm3d-master/utilities.cpp" "/home/hassan/Desktop/TFM/bm3d-master/build/CMakeFiles/bm3d.dir/utilities.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../PUBLIC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
